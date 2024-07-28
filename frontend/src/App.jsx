a5ce90bb576c5f97be566f079729f1b0c372f65c
alpha-154 <mrhrakib07@gmail.com>
2024-07-28 18:59:11 +0600

    first message

---
+++ frontend/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
+
+function App() {
+  const playerRef = useRef(null)
+
+  //this video link should be come from the response's `exec` function
+  // `videoURL` variable
+  
+  const videoLink = "http://localhost:8000/uploads/courses/37a8dcec-716a-4300-ad4b-d26164804c4c/index.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,93 @@
+import express from "express"
+import cors from "cors"
+import multer from "multer"
+import { v4 as uuidv4 } from "uuid"
+import path from "path"
+import fs from "fs"
+import { exec } from "child_process"   //cautious
+
+
+const app = express()
+
+
+//multer middleware
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+      cb(null, "./uploads")
+    },
+    filename: function(req, file, cb){
+      cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
+    }
+  })
+
+//multer configuration
+const upload = multer({ storage: storage})
+
+
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "http://localhost:5173"],
+      credentials: true
+    })
+  )
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*") // watch it
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next()
+  })
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+//learn about it
+app.use("/uploads", express.static("uploads"))
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "hello"})
+})
+
+app.post("/upload", upload.single('file'), function(req, res){
+    const lessonId = uuidv4()
+    const videoPath = req.file.path
+    console.log("videoPath: ", videoPath )
+    const outputPath = `./uploads/courses/${lessonId}`
+    const hlsPath = `${outputPath}/index.m3u8`
+    console.log("hlsPath", hlsPath)
+  
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath, {recursive: true})
+    }
+  
+    // ffmpeg
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}
+  
+  `;
+  
+    // no queue because of POC, not to be used in production
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`exec error: ${error}`)
+      }
+      console.log(`stdout: ${stdout}`)
+      console.log(`stderr: ${stderr}`)
+      const videoUrl = `http://localhost:8000/uploads/courses/${lessonId}/index.m3u8`;
+  
+      res.json({
+        message: "Video converted to HLS format",
+        videoUrl: videoUrl,
+        lessonId: lessonId
+      })
+    })
+  
+  })
+  
+  
+
+
+app.listen(8000, () => {
+    console.log("app is listening at port : 8000")
+})
